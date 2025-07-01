6f7b5b51426e2a7405a7ea27c1c7be9e2a5badde
Romulolevy <romulolevy437@gmail.com>
2025-07-01 15:12:22 -0300

    add new folder

---
+++ excecoes/Prova.java
@@ -0,0 +1,26 @@
+package excecoes;
+
+public class Prova {
+    
+    public static void main(String[] args) {
+        int respostas[] = {0,1,3,2,5,1,2,2,3,0};
+        int contador[] = new int[6];
+
+        for (int i = 0; i < contador.length; i++) {
+            contador[i] = 0;
+        }
+
+        for (int resp : respostas) {
+            try {
+                contador[resp]++;
+            } catch (ArrayIndexOutOfBoundsException excecao) {
+                System.out.println("A resposta " + resp + "nao eh valida e nao foi contabilizada");
+            }
+        }
+
+        for (int i = 0; i < contador.length; i++){
+            System.err.println("Arespstas " + i + " ocorreu " + contador[i] + " vezes ");
+        }
+
+    }
+}
